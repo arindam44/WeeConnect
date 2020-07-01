bf9e559e3dc2777f921911994c1be1525d136330
arindam44 <rarindam68@gmail.com>
2020-07-01 18:30:26 +0530

    resolved adminConfig and gCloudConfig location

--- Config/adminConfig.js
+++ Config/adminConfig.js
@@ -1,10 +1,10 @@
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 
-const serviceAccount = require('../Assets/weconnect-7a79a-firebase-adminsdk-ruueo-c11607d124.json');
+const serviceAccount = require("../Assets/weconnect-7a79a-firebase-adminsdk-ruueo-c11607d124.json");
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    storageBucket: 'weconnect-7a79a.appspot.com'
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: "weconnect-7a79a.appspot.com",
 });
 
 module.exports = admin;
