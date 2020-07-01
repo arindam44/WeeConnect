6f7bf890bdac3d90d6823542a91162d00012ba3d
arindam44 <rarindam68@gmail.com>
2020-07-01 19:42:44 +0530

    added .replace with private_key in adminConfig

--- Config/adminConfig.js
+++ Config/adminConfig.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 admin.initializeApp({
   credential: admin.credential.cert({
     project_id: process.env.PROJECT_ID,
-    private_key: process.env.PRIVATE_KEY,
+    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
     client_email: process.env.CLIENT_EMAIL,
   }),
   storageBucket: "weconnect-7a79a.appspot.com",
