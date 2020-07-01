cf32d0b10ceceaf70c1067e1fb6429987eac1e10
arindam44 <rarindam68@gmail.com>
2020-07-01 14:17:55 +0530

    after Build

---
+++ Client/build/precache-manifest.d12fea9a197daaebfdb09a1f530b0463.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b667e9d1d34fe605fa2094cbf041f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74b3ee4caf231c139bf2",
+    "url": "/static/css/main.2964ecb1.chunk.css"
+  },
+  {
+    "revision": "4d7932c8cd5477d30f1c",
+    "url": "/static/js/2.7feeab3b.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.7feeab3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74b3ee4caf231c139bf2",
+    "url": "/static/js/main.d05408e8.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  },
+  {
+    "revision": "ade859965291410764e04989282e771f",
+    "url": "/static/media/logo.ade85996.png"
+  },
+  {
+    "revision": "77f5794e2eb49f7989b8f85e92cfa4e0",
+    "url": "/static/media/no-image.77f5794e.png"
+  }
+]);
