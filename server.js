131af463e92a7c972a65dfd60de0b5b7e698a96d
arindam44 <rarindam68@gmail.com>
2020-07-02 12:16:13 +0530

    Corrected typo in app.get('/*')

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "Client/build")));
 
   app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "CLient/build/index.html"));
+    res.sendFile(path.join(__dirname, "Client/build/index.html"));
   });
 }
 
