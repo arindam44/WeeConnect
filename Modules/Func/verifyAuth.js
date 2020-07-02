4ea4f350cfb064de4ae2a67652de21d68d8334bf
arindam44 <rarindam68@gmail.com>
2020-07-03 04:00:39 +0530

    changed token verify to jwt

--- Modules/Func/verifyAuth.js
+++ Modules/Func/verifyAuth.js
@@ -1,8 +1,35 @@
 const admin = require("../../Config/adminConfig");
 let user = require("../../Models/users.model");
-
+const jwt = require("jsonwebtoken");
 let idToken;
 
+const verifyAuth = (req, res, next) => {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    idToken = req.headers.authorization.split("Bearer ")[1];
+  } else {
+    console.error("No token found");
+    return res.status(403).json({ error: "Unauthorized" });
+  }
+  const decodedToken = jwt.decode(idToken);
+  console.log(decodedToken);
+  req.user = decodedToken;
+  user
+    .findOne({ fbAuthId: req.user.user_id })
+    .then((data) => {
+      req.user.userHandle = data.userHandle;
+      req.user.imageUrl = data.imageUrl;
+      return next();
+    })
+    .catch((err) => {
+      console.error("Error while verifying token!");
+      return res.status(403).json({ error: "Error while verifying token!" });
+    });
+};
+
+/*
 const verifyAuth = (req, res, next) => {
   console.log(req.headers);
   if (
@@ -29,6 +56,6 @@ const verifyAuth = (req, res, next) => {
       console.error("Error while verifying token!");
       return res.status(403).json({ error: "Error while verifying token!" });
     });
-};
+};*/
 
 module.exports = verifyAuth;
