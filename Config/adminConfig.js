3187363f66b61bbd6796b2d6a5238f0e53f1fda0
arindam44 <rarindam68@gmail.com>
2020-07-02 22:30:07 +0530

    added JSON.parse in private_key

--- Config/adminConfig.js
+++ Config/adminConfig.js
@@ -2,13 +2,23 @@ const admin = require("firebase-admin");
 
 require("dotenv").config();
 
-admin.initializeApp({
-  credential: admin.credential.cert({
-    project_id: process.env.PROJECT_ID,
-    private_key: process.env.PRIVATE_KEY.replace(/\n/g, "\n"),
-    client_email: process.env.CLIENT_EMAIL,
-  }),
-  storageBucket: "weconnect-7a79a.appspot.com",
-});
-
+if (process.env.NODE_ENV === "production") {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      project_id: process.env.PROJECT_ID,
+      private_key: JSON.parse(process.env.PRIVATE_KEY),
+      client_email: process.env.CLIENT_EMAIL,
+    }),
+    storageBucket: "weconnect-7a79a.appspot.com",
+  });
+} else {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      project_id: process.env.PROJECT_ID,
+      private_key: process.env.PRIVATE_KEY.replace(/\n/g, "\n"),
+      client_email: process.env.CLIENT_EMAIL,
+    }),
+    storageBucket: "weconnect-7a79a.appspot.com",
+  });
+}
 module.exports = admin;
