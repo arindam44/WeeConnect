001f1647eeac417e54bc0dd9792125804c4b08d7
arindam44 <rarindam68@gmail.com>
2020-07-02 23:18:38 +0530

    added all env variables in adminConfig

--- Config/adminConfig.js
+++ Config/adminConfig.js
@@ -5,9 +5,16 @@ require("dotenv").config();
 if (process.env.NODE_ENV === "production") {
   admin.initializeApp({
     credential: admin.credential.cert({
+      type: process.env.type,
       project_id: process.env.PROJECT_ID,
+      private_key_id: process.env.private_key_id,
       private_key: JSON.parse(process.env.PRIVATE_KEY),
       client_email: process.env.CLIENT_EMAIL,
+      client_id: process.env.client_id,
+      auth_uri: process.env.auth_uri,
+      token_uri: process.env.token_uri,
+      auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+      client_x509_cert_url: process.env.client_x509_cert_url,
     }),
     storageBucket: "weconnect-7a79a.appspot.com",
   });
