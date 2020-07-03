12b10dbd2c49e70a4a35aa96062672040a1c48b9
arindam44 <rarindam68@gmail.com>
2020-07-03 21:12:27 +0530

    added gcloud_base64

--- Config/gCloudConfig.js
+++ Config/gCloudConfig.js
@@ -1,14 +1,17 @@
 const { Storage } = require("@google-cloud/storage");
 const path = require("path");
-//const servicekey = require("../Assets/weconnect-7a79a-7722933295ec.json");
+//
 
 let storage = null;
 if (process.env.NODE_ENV === "production") {
   storage = new Storage({
     projectId: process.env.PROJECT_ID,
-    keyFilename: process.env.gcloud_base64,
+    keyFilename: JSON.parse(
+      Buffer.from(process.env.gcloud_base64, "base64").toString("ascii")
+    ),
   });
 } else {
+  const servicekey = require("../Assets/weconnect-7a79a-7722933295ec.json");
   storage = new Storage({
     projectId: "weconnect-7a79a",
     keyFilename: servicekey,
