b349e5afef9f13cc47e1423e50b24e3e4f75af1c
arindam44 <rarindam68@gmail.com>
2020-07-04 23:47:12 +0530

    fixed gcloudConfig typo

--- Config/gCloudConfig.js
+++ Config/gCloudConfig.js
@@ -1,7 +1,7 @@
 const { Storage } = require("@google-cloud/storage");
 const path = require("path");
 //
-/*
+
 let storage = null;
 if (process.env.NODE_ENV === "production") {
   storage = new Storage({
@@ -10,11 +10,12 @@ if (process.env.NODE_ENV === "production") {
       Buffer.from(process.env.gcloud_base64, "base64").toString("ascii")
     ),
   });
-} else {*/
-const servicekey = require("../Assets/weconnect-7a79a-7722933295ec.json");
-const storage = new Storage({
-  projectId: "weconnect-7a79a",
-  keyFilename: servicekey,
-});
+} else {
+  const servicekey = require("../Assets/weconnect-7a79a-7722933295ec.json");
+  storage = new Storage({
+    projectId: "weconnect-7a79a",
+    keyFilename: servicekey,
+  });
+}
 
 module.exports = storage;
