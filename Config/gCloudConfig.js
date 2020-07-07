fc63ade82f1638d63f1450a039bf6e88a96d786d
arindam44 <rarindam68@gmail.com>
2020-07-07 23:50:27 +0530

    changed location of gloudConfig

--- Config/gCloudConfig.js
+++ Config/gCloudConfig.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 let storage = null;
-const servicekey = path.resolve(__dirname, "../gcpConfig.json");
+const servicekey = path.resolve(__dirname, "./gcpConfig.json");
 storage = new Storage({
   projectId: "weconnect-7a79a",
   keyFilename: servicekey,
